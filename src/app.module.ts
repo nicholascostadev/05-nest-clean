37d369243db0f4720c0e9194ec23903100bf0357
Nicholas Costa <nicholascostadev@gmail.com>
2025-06-19 18:48:16 -0300

    feat: create initial route for creating an account

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { z } from 'zod/v4';
+import { createErrorMap } from 'zod-validation-error/v4';
+
+z.config({
+  customError: createErrorMap({
+    includePath: true,
+  }),
+});
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}
